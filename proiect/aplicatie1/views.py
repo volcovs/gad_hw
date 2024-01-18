3c63812206e7ba17d71f76d0e4736c0124cc2cfe
volcov_s <volcovsabina@gmail.com>
2024-01-18 20:52:24 +0200

    django part II

--- proiect/aplicatie1/views.py
+++ proiect/aplicatie1/views.py
@@ -1,31 +1,61 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import ListView, CreateView, UpdateView
 
+from aplicatie1.forms import LocationForm
 from aplicatie1.models import Location
 
 
 # Create your views here.
-class LocationView(ListView):
+class LocationView(LoginRequiredMixin, ListView):
     model = Location
     template_name = 'aplicatie1/location_index.html'
 
 
-class CreateLocationView(CreateView):
+class CreateLocationView(LoginRequiredMixin, CreateView):
     model = Location
-    fields = ['city', 'country']
+    # fields = ['city', 'country']
+    form_class = LocationForm
     template_name = 'aplicatie1/location_form.html'
+
+    def get_form_kwargs(self):
+        data = super(CreateLocationView, self).get_form_kwargs()
+        data.update({'pk': None})
+
+        return data
 
     def get_success_url(self):
         return reverse('aplicatie1:lista_locatii')
 
 
-class UpdateLocationView(UpdateView):
+class UpdateLocationView(LoginRequiredMixin, UpdateView):
     model = Location
-    fields = ['city', 'country']
+    # fields = ['city', 'country']
+    form_class = LocationForm
     template_name = 'aplicatie1/location_form.html'
+
+    def get_form_kwargs(self):
+        data = super(UpdateLocationView, self).get_form_kwargs()
+        data.update({'pk': self.kwargs['pk']})
+
+        return data
 
     def get_success_url(self):
         return reverse('aplicatie1:lista_locatii')
 
 
+@login_required
+def deactivate_location(request, pk):
+    Location.objects.filter(id=pk).update(active=0)
+
+    return redirect('aplicatie1:lista_locatii')
+
+
+@login_required
+def activate_location(request, pk):
+    Location.objects.filter(id=pk).update(active=1)
+
+    return redirect('aplicatie1:lista_locatii')
--- proiect/proiect/settings.py
+++ proiect/proiect/settings.py
@@ -116,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/location/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
